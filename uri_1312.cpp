a677b0fe68e3aa975114354c721f2f8f6d653dc5
David Batista <david.batista3010@gmail.com>
2016-12-01 15:52:11 -0200

    Accepted
    1312

---
+++ uri_1312.cpp
@@ -0,0 +1,91 @@
+/// David Mateus Batista <david.batista3010@gmail.com>
+/// Computer Science - Federal University of Itajuba - Brazil
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+#define INF 0x3F3F3F3F
+#define LINF 0x3F3F3F3F3F3F3F3FLL
+#define DINF (double)1e+30
+#define EPS (double)1e-9
+#define PI (double)acos(-1.0)
+#define RAD(x) (double)(x*PI)/180.0
+#define PCT(x,y) (double)x*100.0/y
+#define pb push_back
+#define mp make_pair
+#define pq priority_queue
+#define F first
+#define S second
+#define D(x) x&(-x)
+#define ALL(x) x.begin(),x.end()
+#define RESET(a,b) memset(a, b, sizeof(a))
+#define DEBUG(x,y) cout << x << y << endl
+#define gcd(x,y) __gcd(x, y)
+#define lcm(x,y) (x/gcd(x,y))*y
+#define bitcnt(x) __builtin_popcountll(x)
+#define lbit(x) 63-__builtin_clzll(x)
+#define zerosbitll(x) __builtin_ctzll(x)
+#define zerosbit(x) __builtin_ctz(x)
+
+enum {North, East, South, West};
+//{0, 1, 2, 3}
+//{Up, Right, Down, Left}
+
+int mi[] = {-1, 0, 1, 0, -1, 1, 1, -1};
+int mj[] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+const int MN=1e+3+35;
+int n, k;
+int data[MN][MN];
+int sum[MN][MN];
+int dp[MN][MN][2];
+
+void init()
+{
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+	cout << "[FREOPEN]" << endl;
+	return;
+}
+
+int main()
+{
+	//init();
+	while(scanf("%d", &n) && n)
+	{
+		for(int i=1; i<=n; i++)
+			for(int j=1; j<=i; j++)
+				scanf("%d", &data[i][j]);
+
+		for(int i=1; i<=n; i++)
+			for(int j=1; j<=i; j++)
+				sum[i][j]=(sum[i-1][j]+sum[i-1][j-1]-(i-2>=0?sum[i-2][j-1]:0))+data[i][j];
+
+		int ans=0;
+		for(int i=1; i<=n; i++)
+			for(int j=1; j<=n; j++)
+				dp[i][j][0]=dp[i][j][1]=0;
+
+		for(int j=1; j<=n; j++)
+		{
+			for(int i=j; i<=n; i++)
+			{
+				dp[i][j][0]=sum[i][j];
+				dp[i][j][0]=max(dp[i][j][0], dp[i-1][j-1][1]+(sum[i][j]-sum[i-1][j-1]));
+				ans=max(ans, dp[i][j][0]);
+			}
+			dp[n][j][1]=dp[n][j][0];
+			for(int i=n-1; i>=j; i--)
+				dp[i][j][1]=max(dp[i+1][j][1], dp[i][j][0]);
+		}
+		printf("%d\n", ans);
+	}
+	return 0;
+}
